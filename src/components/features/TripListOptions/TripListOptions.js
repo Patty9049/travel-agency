5c0bdb93a734ec66c77a10e3cf1ba1380987be6f
patrycja <gumis9049@gmail.com>
2021-02-02 22:56:17 +0100

    Fixed filtering by tag

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -6,13 +6,17 @@ import {Row, Col} from 'react-flexbox-grid';
 
 class TripListOptions extends React.Component {
   handleTags(tag, checked){
+    const {filters} = this.props;
     if(checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
+      // e.currentTarget.checked;
+      filters.tags.push(tag);
       this.props.addTag(tag);
     } else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
+      filters.tags.splice(tag);
       this.props.removeTag(tag);
     }
   }
