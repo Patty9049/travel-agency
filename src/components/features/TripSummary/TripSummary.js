960fd336fe8af52b72dbb61ede96a8cbebbf89b8
patrycja <gumis9049@gmail.com>
2021-02-18 15:33:57 +0100

    TripSummary-tags array test

--- src/components/features/TripSummary/TripSummary.js
+++ src/components/features/TripSummary/TripSummary.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
 import styles from './TripSummary.scss';
 import {Col} from 'react-flexbox-grid';
+import { Link } from 'react-router-dom';
 
 const TripSummary = ({id, image, name, cost, days, tags}) => {
   console.log(tags);
@@ -16,11 +17,14 @@ const TripSummary = ({id, image, name, cost, days, tags}) => {
             <span>{days} days</span>
             <span>from {cost}</span>
           </div>
+          {tags.length > 0 &&
           <div className={styles.tags}>
-            {tags.map(tag => (
-              <span className={styles.tag} key={tag.toString()}>{tag}</span>
-            ))}
-          </div>
+            {tags.map(tag => {
+              return (
+                <span className={styles.tag} key={tag.toString()}>{tag}</span>
+              );
+            })}
+          </div>}
         </article>
       </Link>
     </Col>
@@ -28,12 +32,12 @@ const TripSummary = ({id, image, name, cost, days, tags}) => {
 };
 
 TripSummary.propTypes = {
-  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  image: PropTypes.string,
-  name: PropTypes.string,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   intro: PropTypes.string,
-  cost: PropTypes.string,
-  days: PropTypes.number,
+  cost: PropTypes.string.isRequired,
+  days: PropTypes.number.isRequired,
   tags: PropTypes.array.isRequired,
 };
 
--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -4,8 +4,13 @@ import TripSummary from './TripSummary';
 
 describe('Component TripSummary', () => {
   it('should render without crashing', () => {
-    const tags = [];
-    const component = shallow(<TripSummary titleText='Lorem ipsum' imageSrc='image.jpg' tags={tags}/>);
+    const id = 'ID';
+    const image = 'IMG';
+    const name = 'TOGO';
+    const cost = '400';
+    const days = 9;
+    const tags = ['beach', 'spa', 'childfriendly'];
+    const component = shallow(<TripSummary id={id}  image={image} name={name} cost={cost} days={days} titleText='Lorem ipsum' imageSrc='image.jpg' tags={tags}/>);
     expect(component).toBeTruthy();
     console.log(component.debug());
   });
@@ -14,23 +19,75 @@ describe('Component TripSummary', () => {
     expect(() => shallow(<TripSummary />)).toThrow();
   });
 
-  it('should render correct title and image', () => {
-    const expectedTitle = '';
-    const expectedImage = 'image.jpg';
-    const tags = [];
-    const mySrc = expectedImage;
-    const component = shallow(<TripSummary /*titleText={expectedTitle}*/ imageSrc={expectedImage} tags={tags}/>);
-    const renderedTitle = component.find('h3').text();
-    console.log('rendered title', renderedTitle);
-    expect(renderedTitle).toEqual(expectedTitle);
-    expect(mySrc).toEqual(expectedImage);
+  it('should render correct Link, img and props', () => {
+    const tags = ['beach', 'spa', 'childfriendly'];
+    const id = 'ID';
+    const image = 'undefined';
+    const name = 'undefined';
+    const cost = '400';
+    const days = 9;
+    const expectedTo = `/trip/${id}`;
+    const correctSrc = 'undefined';
+    const correctAlt = 'undefined';
+    const component = shallow(<TripSummary id={id} image={image} name={name} cost={cost} days={days} tags={tags}/>);
+    expect(component.find('.link').prop('to')).toEqual(expectedTo);
+    expect(component.find('img').prop('src')).toEqual(correctSrc);
+    expect(component.find('img').prop('alt')).toEqual(correctAlt);
+
   });
 
-  it('renders correct classNames', () => {
-    // const mockVariants = 'small dummy';
-    // const component = shallow(<TripSummary titleText='Lorem' imageSrc='image.jpg' variant={mockVariants} />);
-    // expect(component.hasClass('component')).toBe(true);
-    // expect(component.hasClass('small')).toBe(true);
-    // expect(component.hasClass('dummy')).toBe(true);
+  it('renders correct tags', () => {
+
+    const tags = ['beach', 'spa', 'childfriendly'];
+    const tag1 = tags[0];
+    const tag2 = tags[1];
+    const tag3 = tags[2];
+    console.log('TAG1', tag1);
+    console.log('TAG2', tag2);
+    console.log('TAG3', tag3);
+    const tag1el = `<span className="tag">${tags[0]}</span>`;
+    const tag2el = `<span className="tag">${tags[1]}</span>`;
+    const tag3el = `<span className="tag">${tags[2]}</span>`;
+    console.log('tag1el', tag1el);
+    console.log('tag2el', tag2el);
+    console.log('tag3el', tag3el);
+    const id = 'ID';
+    const image = 'undefined';
+    const name = 'TOGO';
+    const cost = '400';
+    const days = 9;
+    const component = shallow(<TripSummary id={id} image={image} name={name} cost={cost} days={days} tags={tags}/>);
+    console.log(component.debug());
+    expect(component.find('.tag').at(0).prop('children')).toEqual(tag1);
+    expect(component.find('.tag').at(1).prop('children')).toEqual(tag2);
+    expect(component.find('.tag').at(2).prop('children')).toEqual(tag3);
+    expect(component.find('.tags').prop()).toEqual(tags.tag1el);
+    expect(component.find('.tags').prop()).toEqual(tags.tag2el);
+    expect(component.find('.tags').prop()).toEqual(tags.tag3el);
+
+  });
+
+  it('if array tags doesnt exixst or is empty div className=tags doesnt exist.', () => {
+    const tags = [];
+    const id = 'undefined';
+    const image = 'undefined';
+    const name = 'undefined';
+    const cost = 'undefined';
+    const days = 9;
+    const component = shallow(<TripSummary id={id} image={image} name={name} cost={cost} days={days} tags={tags}/>);
+    console.log(component.debug());
+    expect(component.exists('.tag')).toEqual(component.exists('.tags'));
+  });
+
+  it('if array tags contain min 1 tag div className=tags exist.', () => {
+    const tags = ['beach', 'spa', 'childfriendly'];
+    const id = 'undefined';
+    const image = 'undefined';
+    const name = 'undefined';
+    const cost = 'undefined';
+    const days = 9;
+    const component = shallow(<TripSummary id={id} image={image} name={name} cost={cost} days={days} tags={tags}/>);
+    console.log(component.debug());
+    expect(component.exists('.tag')).toEqual(component.exists('.tags'));
   });
 });
